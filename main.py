59b0c494c313fcadbcdff3b6ac9a0a054c0e93c8
mihiryerande <mihiryerande@gmail.com>
2022-03-16 14:30:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# Problem 10:
+#     Summation of Primes
+#
+# Description:
+#     The sum of the primes below 10 is
+#         2 + 3 + 5 + 7 = 17
+#
+#     Find the sum of all the primes below two million.
+
+def main(n):
+    """
+    Returns the sum of all primes less than `n`.
+
+    Args:
+        n (int): Natural number
+
+    Returns:
+        Sum of all primes less than `n`.
+
+    Raises:
+        AssertError: if incorrect params are given
+    """
+    assert type(n) == int and n > 0
+
+    # Maintain a sieve 'mask' indicating which numbers are prime,
+    #   where index `i` represents number i+1
+    # Faster to step through mask and overwrite,
+    #   rather than checking for divisibility of elements
+    sieve = [True for _ in range(n)]
+    sieve[0] = False  # 1 is not prime
+
+    i = 1  # Start iterating from number 2
+    while i < len(sieve):
+        p = i+1  # Next largest prime
+        for j in range(i+p, len(sieve), p):
+            sieve[j] = False
+        i += 1
+
+    ps = list(map(lambda yi: yi[0]+1, filter(lambda xi: xi[1], enumerate(sieve))))
+    return ps, sum(ps)
+
+
+if __name__ == '__main__':
+    num = int(input('Enter a natural number: '))
+    primes, total = main(num)
+    print('Sum of primes below {}:\n  {}'.format(num, total))
+    # print('Primes below {}:'.format(num))
+    # print('  {}'.format('\n  '.join(map(str, primes))))
